4d950d77804326a5cb68c15ab43388094e159fb4
israel butdam <israelxinobutam@gmail.com>
2022-11-03 18:47:31 +0100

    feat: implement checkbox component

---
+++ src/components/checkbox/checkbox.js
@@ -0,0 +1,29 @@
+import { useId } from "react";
+import { TextField } from "../textfield/textfield";
+import "./checkbox.css";
+
+export function Checkbox(props) {
+  const id = useId();
+  const { children, disabled, id: _, ...inputProps } = props;
+
+  return (
+    <label
+      htmlFor={id}
+      className={["checkbox", disabled ? "checkbox--disabled" : ""]
+        .filter((c) => !!c)
+        .join(" ")}
+      tabIndex={0}
+    >
+      <input
+        type="checkbox"
+        id={id}
+        disabled={disabled}
+        {...inputProps}
+        hidden
+      />
+      <span className="checkbox__check" tabIndex={-1}></span>
+
+      <span className="checkbox__text">{children}</span>
+    </label>
+  );
+}
