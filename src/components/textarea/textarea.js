1bdd1d9dec7d1c48fbfc032e3f51cd8481a4e877
israel butdam <israelxinobutam@gmail.com>
2022-11-03 17:59:38 +0100

    feat: implement textarea component

---
+++ src/components/textarea/textarea.js
@@ -0,0 +1,20 @@
+import "./textarea.css";
+
+export function TextArea(props) {
+  const { label, id, hint, error, className, ...textareaProps } = props;
+  const hasError = !!error;
+  const classNames = ["textarea", className];
+  if (hasError) {
+    classNames.unshift("textarea--error");
+  }
+
+  return (
+    <div className={classNames.join(" ")}>
+      <label htmlFor={id}>{label}</label>
+
+      <textarea id={id} {...textareaProps} />
+
+      {!!hint && <span>{hasError ? error : hint}</span>}
+    </div>
+  );
+}
